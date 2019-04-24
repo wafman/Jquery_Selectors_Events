b6cada66932fe277a3687c3828ba9e0aa7c00dd0
Will Fritts <waf515@gmail.com>
2019-04-24 10:07:28 -0700

    finished feature 1

--- js/app.js
+++ js/app.js
@@ -12,10 +12,16 @@ function Img(url, title, description, keyword, horns) {
 
 $.get('./data/page-1.json', data => {
   console.log(data);
+  let template = $('main');
   data.forEach(data => {
     let img = new Img(data.image_url, data.title, data.description, data.keyword, data.horns);
-    $('h2').append(img.title);
-    $('img').attr('src', `${img.url}`);
-    $('p').append(img.description);
+    template.append(`
+      <section>
+      <h2>${img.title}</h2>
+      <img src="${img.url}" alt="${img.keyword}">
+      <p>${img.description}</p>
+      </section>
+      `
+    );
   });
 });
