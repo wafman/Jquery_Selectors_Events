80804edd8ec8183f04877fec7595c375c360a014
Will Fritts <waf515@gmail.com>
2019-04-24 09:24:48 -0700

    constructor

---
+++ js/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log('javascript connected');
+
+function Img(url, title, description, keyword, horns) {
+  this.url = url;
+  this.title = title;
+  this.description = description;
+  this.keyword = keyword;
+  this.horns = horns;
+}
+
